c3f310d5e1621162af7a55b2cf0f7b036845e2dd
Wen-Bin Luo <b00401062@outlook.com>
2024-06-22 09:58:14 +0000

    Count Number of Nice Subarrays

--- numberOfSubarrays.kt
+++ numberOfSubarrays.kt
@@ -2,17 +2,17 @@ import java.util.LinkedList
 
 fun numberOfSubarrays(nums: IntArray, k: Int): Int {
     var numberOfSubarrays = 0
-    var headIdx = -1
+    var lastEvenNumIdx = -1
     val idxOfOddNums = LinkedList<Int>()
     for ((i, num) in nums.withIndex()) {
         if (num % 2 == 1) {
             idxOfOddNums.add(i)
         }
         if (idxOfOddNums.size == k + 1) {
-            headIdx = idxOfOddNums.remove()
+            lastEvenNumIdx = idxOfOddNums.remove()
         }
         if (idxOfOddNums.size == k) {
-            numberOfSubarrays += (idxOfOddNums.first() - headIdx)
+            numberOfSubarrays += (idxOfOddNums.first() - lastEvenNumIdx)
         }
     }
     return numberOfSubarrays
