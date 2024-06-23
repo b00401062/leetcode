6f44ee56e1bcee145532224dacc7cc22702a5006
Wen-Bin Luo <b00401062@outlook.com>
2024-06-23 05:04:55 +0000

    Unique Paths

---
+++ uniquePaths.kt
@@ -0,0 +1,11 @@
+fun choose(n: Int, k: Int): Int {
+    var res = 1L
+    for (i in 0 until k) {
+        res *= (n - i) / (i + 1)
+    }
+    return res.toInt()
+}
+
+fun uniquePaths(m: Int, n: Int): Int {
+    return choose(m + n - 2, min(m - 1, n - 1))
+}
