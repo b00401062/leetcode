7057a3077ecdb4617d707e2e50cba8196a1898ff
Wen-Bin Luo <b00401062@outlook.com>
2024-06-24 02:01:59 +0000

    Median of Two Sorted Arrays

--- addTwoNumbers.kt
+++ addTwoNumbers.kt
@@ -1,6 +1,7 @@
 fun carryRecursively(l: ListNode?): ListNode? {
-    if (l == null)
+    if (l == null) {
         return null
+    }
     if (l.`val` >= 10) {
         l.`val` -= 10
         l.next = l.next ?: ListNode(0)
@@ -11,13 +12,14 @@ fun carryRecursively(l: ListNode?): ListNode? {
 }
 
 fun addTwoNumbersRecursively(l1: ListNode?, l2: ListNode?): ListNode? {
-    if (l1 == null)
-        return l2
-    if (l2 == null)
-        return l1
-    val l = ListNode(l1.`val` + l2.`val`)
-    l.next = addTwoNumbers(l1.next, l2.next)
-    return l
+    return when {
+        l1 == null -> l2
+        l2 == null -> l1
+        else -> ListNode(
+            l1.`val` + l2.`val`,
+            addTwoNumbers(l1.next, l2.next)
+        )
+    }
 }
 
 fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
---
+++ findMedianSortedArrays.kt
@@ -0,0 +1,38 @@
+fun select(l1: IntArray, l2: IntArray, k: Int): Double {
+    if (l1.isEmpty()) {
+        return l2[k].toDouble()
+    }
+    if (l2.isEmpty()) {
+        return l1[k].toDouble()
+    }
+    val pivot = l1[l1.size / 2]
+    val index = l2.binarySearch(pivot)
+    val ltPivotInL1Cnt = l1.size / 2
+    val ltPivotInL2Cnt = if (index >= 0) index else -index - 1
+    val ltPivotCnt = ltPivotInL1Cnt + ltPivotInL2Cnt
+    return when {
+        k < ltPivotCnt -> {
+            val subL1 = l1.sliceArray(0 until ltPivotInL1Cnt)
+            val subL2 = l2.sliceArray(0 until ltPivotInL2Cnt)
+            select(subL1, subL2, k)
+        }
+        k > ltPivotCnt -> {
+            val subL1 = l1.sliceArray(ltPivotInL1Cnt + 1 until l1.size)
+            val subL2 = l2.sliceArray(ltPivotInL2Cnt until l2.size)
+            select(subL1, subL2, k - ltPivotCnt - 1)
+        }
+        else -> pivot.toDouble()
+    }
+}
+
+fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+    val len = nums1.size + nums2.size
+    return when(len % 2) {
+        0 -> {
+            val ltMed = select(nums1, nums2, len / 2 - 1)
+            val rtMed = select(nums1, nums2, len / 2)
+            (ltMed + rtMed) / 2
+        }
+        else -> select(nums1, nums2, len / 2)
+    }
+}
