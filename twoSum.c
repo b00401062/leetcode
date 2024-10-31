f3b6517d3df480753f4ce8f631d14fcba3dd8575
Wen-Bin Luo <b00401062@outlook.com>
2024-10-31 04:42:49 +0000

    1. Two Sum

--- twoSum.c
+++ twoSum.c
@@ -1,51 +1,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define HASHTABLECAPACITY 100003
+#define TABLE_CAPACITY 100003
 
 typedef struct {
     int key;
     int value;
-} HashTableEntry;
+} TableEntry;
 
-typedef HashTableEntry** HashTable;
+typedef TableEntry** Table;
 
-HashTable table_init() {
-    return calloc(sizeof(HashTableEntry*), HASHTABLECAPACITY);
+Table table_init() {
+    return calloc(sizeof(TableEntry*), TABLE_CAPACITY);
 }
 
-int* table_get(HashTable table, int key) {
-    int hash = (key % HASHTABLECAPACITY + HASHTABLECAPACITY) % HASHTABLECAPACITY;
+int* table_get(Table table, int key) {
+    int hash = (key % TABLE_CAPACITY + TABLE_CAPACITY) % TABLE_CAPACITY;
     int* value = NULL;
     while (table[hash] != NULL) {
         if (table[hash]->key == key) {
             value = malloc(sizeof(int));
             *value = table[hash]->value;
         }
-        hash = (hash + 1) % HASHTABLECAPACITY;
+        hash = (hash + 1) % TABLE_CAPACITY;
     }
     return value;
 }
 
-void table_put(HashTable table, int key, int value) {
-    int hash = (key % HASHTABLECAPACITY + HASHTABLECAPACITY) % HASHTABLECAPACITY;
+void table_put(Table table, int key, int value) {
+    int hash = (key % TABLE_CAPACITY + TABLE_CAPACITY) % TABLE_CAPACITY;
     while (table[hash] != NULL) {
-        hash = (hash + 1) % HASHTABLECAPACITY;
+        hash = (hash + 1) % TABLE_CAPACITY;
     }
-    table[hash] = malloc(sizeof(HashTableEntry));
+    table[hash] = malloc(sizeof(TableEntry));
     table[hash]->key = key;
     table[hash]->value = value;
 }
 
-void table_free(HashTable table) {
-    for (int i = 0; i < HASHTABLECAPACITY; i++) {
+void table_free(Table table) {
+    for (int i = 0; i < TABLE_CAPACITY; i++) {
         free(table[i]);
     }
     free(table);
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    HashTable idxOfNums = table_init();
+    Table idxOfNums = table_init();
     for (int i = 0; i < numsSize; i++) {
         int num = nums[i];
         int* j = table_get(idxOfNums, target - num);
