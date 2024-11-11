ebb6104c99d7d5e9703ec0f50e3c51fd909646d3
Wen-Bin Luo <b00401062@outlook.com>
2024-11-11 12:41:57 +0000

    1114. Print in Order

--- Foo.c
+++ Foo.c
@@ -2,37 +2,37 @@
 #include <pthread.h>
 
 typedef struct {
-    pthread_mutex_t mutex1;
-    pthread_mutex_t mutex2;
+    pthread_mutex_t lock1;
+    pthread_mutex_t lock2;
 } Foo;
 
 Foo* fooCreate() {
     Foo* obj = malloc(sizeof(Foo));
-    pthread_mutex_init(&obj->mutex1, NULL);
-    pthread_mutex_init(&obj->mutex2, NULL);
-    pthread_mutex_lock(&obj->mutex1);
-    pthread_mutex_lock(&obj->mutex2);
+    pthread_mutex_init(&obj->lock1, NULL);
+    pthread_mutex_init(&obj->lock2, NULL);
+    pthread_mutex_lock(&obj->lock1);
+    pthread_mutex_lock(&obj->lock2);
     return obj;
 }
 
 void first(Foo* obj) {
     printFirst();
-    pthread_mutex_unlock(&obj->mutex1);
+    pthread_mutex_unlock(&obj->lock1);
 }
 
 void second(Foo* obj) {
-    pthread_mutex_lock(&obj->mutex1);
+    pthread_mutex_lock(&obj->lock1);
     printSecond();
-    pthread_mutex_unlock(&obj->mutex2);
+    pthread_mutex_unlock(&obj->lock2);
 }
 
 void third(Foo* obj) {
-    pthread_mutex_lock(&obj->mutex2);
+    pthread_mutex_lock(&obj->lock2);
     printThird();
 }
 
 void fooFree(Foo* obj) {
-    pthread_mutex_destroy(&obj->mutex1);
-    pthread_mutex_destroy(&obj->mutex2);
+    pthread_mutex_destroy(&obj->lock1);
+    pthread_mutex_destroy(&obj->lock2);
     free(obj);
 }
