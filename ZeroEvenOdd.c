5cf6c6fc32cc7a2ef054409aef1fd3ffd2eacabb
Wen-Bin Luo <b00401062@outlook.com>
2024-11-16 15:18:28 +0000

    1116. Print Zero Even Odd

--- ZeroEvenOdd.c
+++ ZeroEvenOdd.c
@@ -10,41 +10,41 @@ ZeroEvenOdd* zeroEvenOddCreate(int n) {
     ZeroEvenOdd* obj = malloc(sizeof(ZeroEvenOdd));
     obj->n = n;
     for (int i = 0; i < 3; i++) {
-        pthread_mutex_init(obj->locks + i, NULL);
+        pthread_mutex_init(&obj->locks[i], NULL);
     }
     for (int i = 0; i < 2; i++) {
-        pthread_mutex_lock(obj->locks + i);
+        pthread_mutex_lock(&obj->locks[i]);
     }
     return obj;
 }
 
 void zero(ZeroEvenOdd* obj) {
     for (int i = 1; i <= obj->n; i++) {
-        pthread_mutex_lock(obj->locks + 2);
+        pthread_mutex_lock(&obj->locks[2]);
         printNumber(0);
-        pthread_mutex_unlock(obj->locks + i % 2);
+        pthread_mutex_unlock(&obj->locks[i % 2]);
     }
 }
 
 void even(ZeroEvenOdd* obj) {
     for (int i = 2; i <= obj->n; i += 2) {
-        pthread_mutex_lock(obj->locks);
+        pthread_mutex_lock(&obj->locks[0]);
         printNumber(i);
-        pthread_mutex_unlock(obj->locks + 2);
+        pthread_mutex_unlock(&obj->[2]);
     }
 }
 
 void odd(ZeroEvenOdd* obj) {
     for (int i = 1; i <= obj->n; i += 2) {
-        pthread_mutex_lock(obj->locks + 1);
+        pthread_mutex_lock(&obj->locks[1]);
         printNumber(i);
-        pthread_mutex_unlock(obj->locks + 2);
+        pthread_mutex_unlock(&obj->locks[2]);
     }
 }
 
 void zeroEvenOddFree(ZeroEvenOdd* obj) {
     for (int i = 0; i < 3; i++) {
-        pthread_mutex_destroy(obj->locks + i);
+        pthread_mutex_destroy(&obj->locks[i]);
     }
     free(obj);
 }
