462157bbf913d3a5648d05f0d224634efc48fe14
Wen-Bin Luo <b00401062@outlook.com>
2024-11-18 01:18:44 +0000

    862. Shortest Subarray with Sum at Least K

--- shortestSubarray.c
+++ shortestSubarray.c
@@ -4,8 +4,8 @@
 #define DEQUE_CAPACITY 100000
 
 #define min(a, b) ({\
-    __typeof__ (a) _a = (a); \
-    __typeof__ (b) _b = (b); \
+    __typeof__(a) _a = (a);\
+    __typeof__(b) _b = (b);\
     _a < _b ? _a : _b;\
 })
 
