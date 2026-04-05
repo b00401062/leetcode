4d22987bfdd04f0e266ff409cfb4dd2215182c96
Wen-Bin Luo <b00401062@outlook.com>
2026-04-05 02:00:16 +0000

    14. Longest Common Prefix

--- longestCommonPrefix.c
+++ longestCommonPrefix.c
@@ -1,8 +1,14 @@
+#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* reduceToCommonPrefix(char* s1, char* s2) {
-    assert(s1 != NULL && s2 != NULL);
+    assert(s1 != NULL || s2 != NULL);
     char* prefix = (char*) calloc(sizeof(char), 201);
+    if (s1 == NULL || s2 == NULL) {
+        strcpy(prefix, s1 ? s1 : s2);
+        return prefix;
+    }
     for (int i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++) {
         prefix[i] = s1[i];
     }
@@ -11,11 +17,10 @@ char* reduceToCommonPrefix(char* s1, char* s2) {
 
 char* longestCommonPrefix(char** strs, int strsSize) {
     if (strsSize == 0) return "";
-    if (strsSize == 1) return strs[0];
-    char* prefix = strs[0];
-    for (int i = 1; i < strsSize; i++) {
+    char* prefix = NULL;
+    for (int i = 0; i < strsSize; i++) {
         char* ret = reduceToCommonPrefix(prefix, strs[i]);
-        if (prefix != NULL) free(prefix);
+        free(prefix);
         prefix = ret;
     }
     return prefix;
